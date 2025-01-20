2ae7047a8591fbb3b0c88a7d90353f92bda5a491
Melissa <melissaaragaoleite@gmail.com>
2025-01-20 11:13:00 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,15 +22,19 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private double score;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(long id, String title, Integer year, String genre, String platforms, double score, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
@@ -83,11 +87,11 @@ public class Game {
 		this.platforms = platforms;
 	}
 
-	public double getScore() {
+	public Double getScore() {
 		return score;
 	}
 
-	public void setScore(double score) {
+	public void setScore(Double score) {
 		this.score = score;
 	}
 
